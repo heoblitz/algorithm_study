07aea84d7510ea15f1a1263aa64104b151dfcfe7
wone2287 <wone2287@gmail.com>
2020-04-19 23:33:54 +0900

    add problems

--- 백준_알고리즘/1157.py
+++ 백준_알고리즘/1157.py
@@ -2,4 +2,17 @@ hash_dict = {chr(x): 0 for x in range(65, 91)}
 word = input()
 cnt = 0
 
-print(hash_dict)
+for d in word.upper():
+    cnt = hash_dict[d]
+    cnt += 1
+
+    hash_dict[d] = cnt
+
+cnt_list = list(hash_dict.values())
+cnt_list.sort(reverse=True)
+
+if cnt_list[0] != cnt_list[1]:
+    max_key = max(hash_dict, key=hash_dict.get)
+    print(max_key)
+else:
+    print("?")
