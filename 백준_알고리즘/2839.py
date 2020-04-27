41fc524a322abff4c9fcc931acc95c33569a60cd
wone2287 <wone2287@gmail.com>
2020-04-27 23:01:26 +0900

    fix 2839.py

--- 백준_알고리즘/2839.py
+++ 백준_알고리즘/2839.py
@@ -1,25 +1,20 @@
 weight = int(input())
 cnt = 0
 
-for i in [5, 3]:
-    if weight <= 0:
+while weight > 0:
+    if weight % 5 == 0:
+        cnt += weight // 5
+        weight = 0
         break
+    else:
+        weight -= 3
+        cnt += 1
 
-    current_cnt = weight // i
-
-    if (( weight - ( current_cnt * i )) < 3 and ( weight - ( current_cnt * i )) != 0 ) :
-        current_cnt -= 1
-    
-    weight -= current_cnt * i
-    cnt += current_cnt
-
-    print(weight, cnt, current_cnt)
-    
 if weight == 0:
     print(cnt)
-    
 else:
     print("-1")
+
 
 # 나머지가 3 이하일 때 i 를 하나 깍아도 안되는 케이스가 발생한다
 # 3과 5의 최대 공배수 를 생각해봐야 할듯..
